72824d91c448a9a21068ae90815550975d6007d8
zhaoyulong <scp3125@qq.com>
2022-05-08 18:00:56 +0800

    增加中文字体测试

--- src/main.rs
+++ src/main.rs
@@ -24,7 +24,30 @@ fn main() {
 }
 
 fn ui_example(mut egui_context: ResMut<EguiContext>) {
+    let mut fonts = egui::FontDefinitions::default();
+
+    // Install my own font (maybe supporting non-latin characters).
+    // .ttf and .otf files supported.
+    fonts.font_data.insert(
+        "my_font".to_owned(),
+        egui::FontData::from_static(include_bytes!("../assets/wqy-microhei.ttc")),
+    );
+
+    // Put my font first (highest priority) for proportional text:
+    fonts
+        .families
+        .entry(egui::FontFamily::Proportional)
+        .or_default()
+        .insert(0, "my_font".to_owned());
+
+    // Put my font as last fallback for monospace:
+    fonts
+        .families
+        .entry(egui::FontFamily::Monospace)
+        .or_default()
+        .push("my_font".to_owned());
+    egui_context.ctx_mut().set_fonts(fonts);
     egui::Window::new("Hello").show(egui_context.ctx_mut(), |ui| {
-        ui.label("中文");
+        ui.label("这是中文");
     });
 }
